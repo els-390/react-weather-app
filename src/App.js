d12a444b5ff4c277efcafdeff686e45dd0a0dcf4
els-390 <103050889+els-390@users.noreply.github.com>
2022-12-15 19:16:23 +0000

    Added html content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was created by
+          <a
+            href="https://www.linkedin.com/in/ellie-s-51088146/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Ellie Spanswick
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/els-390/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
